b963955a852b0727f0182fba28c7f012e3181b53
stellalau <stellalau@aptina.com>
2015-03-18 16:11:43 -0700

    Add invalid test cases for Video and Network tests. Refactor the verify response body to put it in a function.

--- src/com/onsemi/matrix/api/NetworkTest.java
+++ src/com/onsemi/matrix/api/NetworkTest.java
@@ -57,8 +57,7 @@ public class NetworkTest {
   public void getlan_dhcpenable() {
 	  printResponse();
 	  assertOk(response);
-	  String dhcpenable = response.getBody();
-	  assertTrue(dhcpenable.contains("lan_dhcpenable=1"));
+	  verifyResponse("lan_dhcpenable=1");
 	  
   }
   
@@ -82,8 +81,7 @@ public class NetworkTest {
   public void getrtspenable() {
 	  printResponse();
 	  assertOk(response);
-	  String rtspenable = response.getBody();
-	  assertTrue(rtspenable.contains("rtsp_enable=1"));
+	  verifyResponse("rtsp_enable=1");
   }
 
 @HttpTest (
@@ -104,15 +102,27 @@ public void setrtspports() {
 public void getrtspportscount() {
 	  printResponse();
 	  assertOk(response);
-	  String rtspport = response.getBody();
-	  assertTrue(rtspport.contains("rtspports=9000"));
+	  verifyResponse("rtspports=9000");
 	  
 }
+
+@HttpTest (
+		  method = Method.GET,
+		  path ="/vb.htm?rtspports=70000",
+		  authentications = { @Authentication( type = BASIC, user = "admin", password = "admin" ) } ,
+		  order = 6)
+public void setinvalidrtspports() {
+	  printResponse();
+	  assertOk(response);
+	  String rtspport = response.getBody();
+	  assertFalse(rtspport.contains("OK"));
+}
+
 @HttpTest (
 		  method = Method.GET,
 		  path ="/vb.htm?upnp_on=0",
 		  authentications = { @Authentication( type = BASIC, user = "admin", password = "admin" ) } ,
-		  order = 6)
+		  order = 7)
 public void setupnp() {
 	  printResponse();
 	  assertOk(response);
@@ -122,19 +132,31 @@ public void setupnp() {
 		  method = Method.GET,
 		  path ="/vb.htm?paratest=upnp_on",
 		  authentications = { @Authentication( type = BASIC, user = "admin", password = "admin" ) } ,
-		  order = 7)
+		  order = 8)
   public void getupnp() {
 	  printResponse();
 	  assertOk(response);
-	  String upnp = response.getBody();
-	  assertTrue(upnp.contains("upnp_on=0"));
+	  verifyResponse("upnp_on=0");
   }
+  
+ @HttpTest (
+		  method = Method.GET,
+		  path ="/vb.htm?upnp_on=2",
+		  authentications = { @Authentication( type = BASIC, user = "admin", password = "admin" ) } ,
+		  order = 9)
+public void setinvalidupnp() {
+	  printResponse();
+	  assertOk(response);
+	  String upnp = response.getBody();
+	  assertFalse(upnp.contains("OK"));
+	  
+}
   
   @HttpTest (
 		  method = Method.GET,
 		  path ="/vb.htm?upnp_on=1",
 		  authentications = { @Authentication( type = BASIC, user = "admin", password = "admin" ) } ,
-		  order = 8)
+		  order = 10)
 public void setdefaultupnp() {
 	  printResponse();
 	  assertOk(response);
@@ -145,7 +167,7 @@ public void setdefaultupnp() {
 		  method = Method.GET,
 		  path ="/vb.htm?internet_wifi=1",
 		  authentications = { @Authentication( type = BASIC, user = "admin", password = "admin" ) } ,
-		  order = 9)
+		  order = 11)
   public void setinternetwifi() {
 	  printResponse();
 	  assertOk(response);
@@ -155,19 +177,30 @@ public void setdefaultupnp() {
 		  method = Method.GET,
 		  path ="/vb.htm?paratest=internet_wifi",
 		  authentications = { @Authentication( type = BASIC, user = "admin", password = "admin" ) } ,
-		  order = 10)
+		  order = 12)
   public void getinternetwifi() {
 	  printResponse();
 	  assertOk(response);
-	  String internetwifi = response.getBody();
-	  assertTrue(internetwifi.contains("internet_wifi=1"));
+	  verifyResponse("internet_wifi=1");
   }
-  
+
+@HttpTest (
+		  method = Method.GET,
+		  path ="/vb.htm?internet_wifi=3",
+		  authentications = { @Authentication( type = BASIC, user = "admin", password = "admin" ) } ,
+		  order = 13)
+  public void setinvalidinternetwifi() {
+	  	printResponse();
+	  	assertOk(response);
+	  	String internetwifi = response.getBody();
+		assertFalse(internetwifi.contains("OK"));
+  }
+
 @HttpTest (
 		  method = Method.GET,
 		  path ="/vb.htm?internet_wifi=0",
 		  authentications = { @Authentication( type = BASIC, user = "admin", password = "admin" ) } ,
-		  order = 11)
+		  order = 14)
   public void setdefaultinternetwifi() {
 	  printResponse();
 	  assertOk(response);
@@ -177,7 +210,7 @@ public void setdefaultupnp() {
 		  method = Method.GET,
 		  path ="/vb.htm?dynamic_bitrate_fps=1",
 		  authentications = { @Authentication( type = BASIC, user = "admin", password = "admin" ) } ,
-		  order = 12)
+		  order = 15)
 public void setdynamic_bitrate() {
 	  printResponse();
 	  assertOk(response);
@@ -187,19 +220,30 @@ public void setdynamic_bitrate() {
 		  method = Method.GET,
 		  path ="/vb.htm?paratest=dynamic_bitrate_fps",
 		  authentications = { @Authentication( type = BASIC, user = "admin", password = "admin" ) } ,
-		  order = 13)
+		  order = 16)
 public void getidynamicbitrate() {
 	  printResponse();
 	  assertOk(response);
+	  verifyResponse("dynamic_bitrate_fps=1");
+}
+
+@HttpTest (
+		  method = Method.GET,
+		  path ="/vb.htm?dynamic_bitrate_fps=3",
+		  authentications = { @Authentication( type = BASIC, user = "admin", password = "admin" ) } ,
+		  order = 17)
+public void setinvaliddynamic_bitrate() {
+	  printResponse();
+	  assertOk(response);
 	  String dynamicbitrate = response.getBody();
-	  assertTrue(dynamicbitrate.contains("dynamic_bitrate_fps=1"));
+	  assertFalse(dynamicbitrate.contains("OK"));
 }
 
 @HttpTest (
 		  method = Method.GET,
 		  path ="/vb.htm?lan_mask=255.255.000.000",
 		  authentications = { @Authentication( type = BASIC, user = "admin", password = "admin" ) } ,
-		  order = 14)
+		  order = 18)
 public void setlan_mask() {
 	  printResponse();
 	  assertOk(response);	  
@@ -209,18 +253,17 @@ public void setlan_mask() {
 		  method = Method.GET,
 		  path ="/vb.htm?paratest=lan_mask",
 		  authentications = { @Authentication( type = BASIC, user = "admin", password = "admin" ) } ,
-		  order = 15)
+		  order = 19)
 public void getlan_mask() {
 	  printResponse();
 	  assertOk(response);	  
-	  String lan_mask = response.getBody();
-	  assertTrue(lan_mask.contains("lan_mask=255.255.000.000"));
+	  verifyResponse("lan_mask=255.255.000.000");
 }
 @HttpTest (
 		  method = Method.GET,
 		  path ="/vb.htm?lan_mask=255.255.255.000",
 		  authentications = { @Authentication( type = BASIC, user = "admin", password = "admin" ) } ,
-		  order = 16)
+		  order = 20)
 public void setdefaultlanmask() {
 	  printResponse();
 	  assertOk(response);	  
@@ -230,7 +273,7 @@ public void setdefaultlanmask() {
 		  method = Method.GET,
 		  path ="/vb.htm?lan_gateway=001.000.000.000",
 		  authentications = { @Authentication( type = BASIC, user = "admin", password = "admin" ) } ,
-		  order = 17)
+		  order = 21)
 public void setlangateway() {
 	  printResponse();
 	  assertOk(response);	  
@@ -241,19 +284,18 @@ public void setlangateway() {
 		  method = Method.GET,
 		  path ="/vb.htm?paratest=lan_gateway",
 		  authentications = { @Authentication( type = BASIC, user = "admin", password = "admin" ) } ,
-		  order = 18)
+		  order = 22)
 public void getlangateway() {
 	  printResponse();
 	  assertOk(response);
-	  String lan_gateway = response.getBody();
-	  assertTrue(lan_gateway.contains("lan_gateway=001.000.000.000"));
+	  verifyResponse("lan_gateway=001.000.000.000");
 }
 
 @HttpTest (
 		  method = Method.GET,
 		  path ="/vb.htm?lan_gateway=000.000.000.000",
 		  authentications = { @Authentication( type = BASIC, user = "admin", password = "admin" ) } ,
-		  order = 19)
+		  order = 24)
 public void setdefaultlangateway() {
 	  printResponse();
 	  assertOk(response);
@@ -264,7 +306,7 @@ public void setdefaultlangateway() {
 		  method = Method.GET,
 		  path ="/vb.htm?lan_dns1=192.168.001.000",
 		  authentications = { @Authentication( type = BASIC, user = "admin", password = "admin" ) } ,
-		  order = 20)
+		  order = 25)
 public void setdnsl() {
 	  printResponse();
 	  assertOk(response);	  
@@ -274,12 +316,11 @@ public void setdnsl() {
 		  method = Method.GET,
 		  path ="/vb.htm?paratest=lan_dns1",
 		  authentications = { @Authentication( type = BASIC, user = "admin", password = "admin" ) } ,
-		  order = 21)
+		  order = 26)
 public void getdns1() {
 	  printResponse();
 	  assertOk(response);
-	  String dns1 = response.getBody();
-	  assertTrue(dns1.contains("lan_dns1=192.168.001.000"));
+	  verifyResponse("lan_dns1=192.168.001.000");
 	  
 }
 
@@ -287,7 +328,7 @@ public void getdns1() {
 		  method = Method.GET,
 		  path ="/vb.htm?lan_dns1=192.168.001.001",
 		  authentications = { @Authentication( type = BASIC, user = "admin", password = "admin" ) } ,
-		  order = 22)
+		  order = 27)
 public void setdefaultdnsl() {
 	  printResponse();
 	  assertOk(response);	  
@@ -302,4 +343,10 @@ public void setdefaultdnsl() {
 	  System.out.println("===========");
 
   }
+  
+  private void verifyResponse(String verifystr){
+		String body = response.getBody();
+		assertTrue(body.contains(verifystr));
+
+}	
 }
